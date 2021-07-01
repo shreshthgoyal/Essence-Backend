f636717a50ab391243715096424a351f6a4ad4bd
Shreshth Goyal <73751013+shreshthgoyal@users.noreply.github.com>
2021-07-01 19:06:42 +0530

    Update login.js

--- controllers/login.js
+++ controllers/login.js
@@ -29,7 +29,7 @@ exports.login = async (req, res) => {
                             );
                             res.status(200)
                                 .cookie("token", token, {                                                      //Assigning the token to a cookie
-                                    sameSite: 'none', secure: true,
+                                    samesite: 'none', secure: true,
                                     expires: new Date(new Date().getTime() + 5 * 10000),
                                     httpOnly: true,
                                 })
