d36dceaa08dd6c4b9ebc45180830185b33d7ad11
Shreshth Goyal <73751013+shreshthgoyal@users.noreply.github.com>
2021-07-01 19:41:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,16 +12,13 @@ const compression = require('compression')
 
 const app = express();                          //Initialized express
 app.use(express.json());                     
-app.use(cors());
 app.use(cookieParser());
 app.use(helmet());
 app.use(compression());
 
-app.use(function(req, res, next) {  
-  res.header('Access-Control-Allow-Origin', req.headers.origin);
-  res.header("Access-Control-Allow-Credentials", true);
-  next();
-}); 
+
+app.use(cors({origin: "https://essencefest.netlify.app", credentials: true}));
+
 
 const port = process.env.PORT || 5000;
 
