ee0a8d35b21409d22322224a81d301f6d7ee38fa
Shreshth Goyal <73751013+shreshthgoyal@users.noreply.github.com>
2021-07-02 06:07:02 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -9,7 +9,7 @@ const { verifym } = require("../middleware/authmiddleware");
 router.post('/register' , register);                        //POST request to register the user
 router.post('/login' , login);                              // POST request to login the user
 router.put('/verify/:token', verify);                  //PUT request to verify the email of the user
-router.get('/dashboard', verifym, info);              //GET request to get the information for user panel
+router.get('/dashboard', info);              //GET request to get the information for user panel
 
 
 module.exports = router;
