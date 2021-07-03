45e8e5899ef22ca9bb7710eef244429ea72cb8e1
Shreshth Goyal <73751013+shreshthgoyal@users.noreply.github.com>
2021-07-03 16:44:53 +0530

    Update goodies.js

--- controllers/goodies.js
+++ controllers/goodies.js
@@ -2,7 +2,7 @@ const client = require("../configs/database");
 
 exports.goodies = async (req, res) => {
     try {
-        const data = await client.query(`SELECT goodie_id, goodie_name, price, users.id, name, college from goodies inner join testing on goodie_id = goodieid inner join users on users.id = testing.id `)
+        const data = await client.query(`SELECT name, goodie_name from goodies inner join goodies_registration on goodies.goodie_id = goodies_registration.goodie_id inner join users on users.id = user_id `)
             const userData = data.rows;
             const filteredData = userData.map((info) => {
                 return {
