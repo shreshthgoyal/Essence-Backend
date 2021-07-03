e91c2ee019bde7c9082739e5c510f51d10463f7e
Shreshth Goyal <73751013+shreshthgoyal@users.noreply.github.com>
2021-07-03 20:52:03 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -4,12 +4,19 @@ const {register} = require("../controllers/register");
 const {verify} = require("../controllers/verify");
 const {login} = require("../controllers/login");
 const {info} = require("../controllers/info");
+const {event} = require("../controllers/uevent");
+const {pronite} = require("../controllers/upronite");
+const {goodies} = require("../controllers/ugoodies");
 const { verifym } = require("../middleware/authmiddleware");
+
 
 router.post('/register' , register);                        //POST request to register the user
 router.post('/login' , login);                              // POST request to login the user
 router.put('/verify/:token', verify);                  //PUT request to verify the email of the user
 router.get('/dashboard', verifym, info);              //GET request to get the information for user panel
+router.get('/event', verifym, event);              //GET request to get the information for user panel
+router.get('/pronite', verifym, pronite);              //GET request to get the information for user panel
+router.get('/goodies', verifym, goodies);              //GET request to get the information for user panel
 
 
 module.exports = router;
