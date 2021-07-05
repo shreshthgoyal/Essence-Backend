618ff1be2f61a84723b9143af1249498c26588ee
Rishabh Kumar <74629127+rish78@users.noreply.github.com>
2021-07-06 04:53:21 +0530

    Update authmiddleware.js

--- middleware/authmiddleware.js
+++ middleware/authmiddleware.js
@@ -4,12 +4,20 @@ const client = require("../configs/database");
 //Verifying the JSON web token
 
 exports.verifym = (req,res,next) => {
-    const token = req.headers.authorization;
+       const token = req.headers.authorization;
+       if(!token){
+        res.status(401).json({
+          message: "Invalid token! Please signin/signup first.",
+        });
+       }
        
     jwt.verify(token, process.env.SECRET_KEY, (err,decoded)=>{
         if(err)
         {
           console.log(err); 
+          res.status(400).json({
+            message: "Invalid token",
+          });
         }
 
         else{
@@ -18,11 +26,12 @@ exports.verifym = (req,res,next) => {
         .query(`SELECT * FROM users WHERE email = $1;`,[userEmail])
         .then((info) => {
             if (info.rows.length === 0) {
-                res.status(400).json({
+                 res.status(400).json({
                   message: "Invalid token",
                 });
               } else {
                 req.email = userEmail;
+                req.userid = info.rows[0].id;
                 next();
               }
         })
