cd5c4e36280e1fc0f4f0ebf852331550f23de663
kshitij-404 <76985867+kshitij-404@users.noreply.github.com>
2021-07-06 19:46:14 +0530

    Update pronites.js

--- routes/pronites.js
+++ routes/pronites.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const { proniteRegister } = require("../controllers/proniteRegn");
-const { proniteUnregister } = require("../controllers/proniteUnregister");
 const { verifym } = require("../middleware/authmiddleware");
 const { proniteIdParam } = require("../middleware/pronitesMiddleware");
 
 router.param("proniteid", proniteIdParam);
 
 router.post("/proniteRegn/:proniteid", verifym, proniteRegister);
-router.delete("/proniteUnregister/:proniteid", verifym, proniteUnregister);
 
 module.exports = router;
